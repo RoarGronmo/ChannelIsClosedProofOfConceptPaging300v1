d29b99795f093c06b338ea68b2be14b2b7b6d244
Roar <roar.gronmo@gmail.com>
2020-07-15 22:17:37 +0200

    Begin pager implementation. 2.

--- app/src/main/java/no/rogo/channelisclosedproofofconceptpaging300v1/repository/pagingsources/StationPagingSource.kt
+++ app/src/main/java/no/rogo/channelisclosedproofofconceptpaging300v1/repository/pagingsources/StationPagingSource.kt
@@ -1,8 +1,12 @@
 package no.rogo.channelisclosedproofofconceptpaging300v1.repository.pagingsources
 
+import android.util.Log
 import androidx.paging.PagingSource
 import no.rogo.channelisclosedproofofconceptpaging300v1.api.interfaces.APIFamappInterfaceService
+import no.rogo.channelisclosedproofofconceptpaging300v1.api.responses.APIGetStationsResponse
 import no.rogo.channelisclosedproofofconceptpaging300v1.room.entities.StationEntity
+import retrofit2.HttpException
+import java.io.IOException
 
 /**
  * Created by Roar on 15.07.2020.
@@ -14,18 +18,52 @@ class StationPagingSource(
         private val passfrase: String,
         private val latitude: String,
         private val longitude: String,
-        private val limit: String,
-        private val offset: String,
-        private val lastversion: String,
+        private val sourceLimit: String,
+        private val sourceOffset: String,
+        private val lastVersion: String,
         private val killed: String,
         private val range: String
-): PagingSource<Int, StationEntity>() {
+): PagingSource<Int, APIGetStationsResponse>() {
+
+    val TAG = javaClass.simpleName
+
     override suspend fun load(
             params: LoadParams<Int>
-    ): LoadResult<Int, StationEntity>
+    ): LoadResult<Int, APIGetStationsResponse>
     {
-        val req = apiFamappInterfaceService.getStations(
+        val limit = params.loadSize.toString()
+        val offset = (params.loadSize*(params.key?:1-1)).toString()
+        val prevKey = if((params.key?:1-1)<=0) null else (params.key?:1-2)
 
-        )
+        Log.i(TAG, "load: limit = $limit, offset = $offset, prevKey = $prevKey")
+        try {
+            val req = apiFamappInterfaceService.getStations(
+                    userid = userId,
+                    passfrase = passfrase,
+                    latitude = latitude,
+                    longitude = longitude,
+                    limit = limit,
+                    offset = offset,
+                    lastversion = lastVersion,
+                    killed = killed,
+                    range = range
+            )
+            val nextKey = if(req.isEmpty()) null else (params.key?:0)+1
+            Log.i(TAG, "load: nextKey = $nextKey")
+            return LoadResult.Page(
+                    data = req,
+                    prevKey = prevKey,
+                    nextKey = nextKey
+            )
+
+        }catch (exception: IOException)
+        {
+            Log.e(TAG,"load: LoadResult Error ",exception )
+            return LoadResult.Error(exception)
+        }catch (exception: HttpException)
+        {
+            Log.e(TAG,"load: LoadResult Http ",exception)
+            return LoadResult.Error(exception)
+        }
     }
 }
