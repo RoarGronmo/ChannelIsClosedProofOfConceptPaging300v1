475f9e377e1bf934be9bc5359ef1edf1de1bf5fc
Roar <roar.gronmo@gmail.com>
2020-07-18 09:31:39 +0200

    Begin pager implementation. 2. 3. 4? 5? 6? 7?

--- app/src/main/java/no/rogo/channelisclosedproofofconceptpaging300v1/repository/mediators/StationRemoteMediator.kt
+++ app/src/main/java/no/rogo/channelisclosedproofofconceptpaging300v1/repository/mediators/StationRemoteMediator.kt
@@ -6,6 +6,7 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
 import no.rogo.channelisclosedproofofconceptpaging300v1.api.factories.APIFamappClientFactory
 import no.rogo.channelisclosedproofofconceptpaging300v1.api.responses.APIGetStationsResponse
 import no.rogo.channelisclosedproofofconceptpaging300v1.room.db.AppDatabase
@@ -35,26 +36,45 @@ class StationRemoteMediator(
         val page = when (loadType)
         {
             LoadType.REFRESH ->{
-
+                val remoteKey = getRemoteKeyForClosestItemToPosition(state)
+                remoteKey?.nextKey?.minus(1)?:1
             }
-            LoadType.PREPEND ->{
-
-            }
-            LoadType.APPEND -> {
-                val remoteKey = getRemoteKeyForLastItem(state)
-                if(remoteKey == null || remoteKey.nextKey == null) {
-                    Log.e(TAG, "load: Remote key or next key should not be null" )
+            LoadType.PREPEND -> {
+                Log.i(TAG, "load: PREPEND state = $state")
+                val remoteKey = getRemoteKeyForFirstItem(state)
+                if (remoteKey == null) {
+                    Log.e(TAG, "load: PREPEND Remote key or next key should not be null")
                     throw InvalidObjectException("Remote key or next key should not be null")
                 }
+                //val prevKey = remoteKey.prevKey
+                if (remoteKey.prevKey == null) {
+                    Log.i(TAG, "load: PREPEND remoteKey.prevKey is null, no more data available ?")
+                    return MediatorResult.Success(endOfPaginationReached = true)
+                }
+                Log.i(TAG, "load: PREPEND sets page = remoteKey.prevKey = ${remoteKey.prevKey}")
+                remoteKey.prevKey
+            }
+            LoadType.APPEND -> {
+                Log.i(TAG, "load: APPEND state = $state")
+                val remoteKey = getRemoteKeyForLastItem(state)
+                if (remoteKey == null || remoteKey.nextKey == null) {
+                    Log.e(TAG, "load: APPEND Remote key or next key should not be null")
+                    throw InvalidObjectException("Remote key or next key should not be null")
+                }
+                Log.i(TAG, "load: APPEND sets page = remoteKey.nextKey = ${remoteKey.nextKey}")
                 remoteKey.nextKey
             }
         }
 
+        Log.i(TAG, "load: page = $page")
+
         val service = APIFamappClientFactory.makeAPIFamappInterfaceService()
 
         try {
-            val limit = "20"
-            val offset = "0"
+            val ilimit= 20
+
+            val limit = ilimit.toString()
+            val offset = (page*(ilimit-1)).toString()
 
             val req = service.getStations(
                 userid = "10000",
@@ -67,6 +87,26 @@ class StationRemoteMediator(
                     limit = limit,
                     offset = offset
             )
+
+            Log.i(TAG, "load: req.size = ${req.size }")
+
+            appDatabase.withTransaction {
+                if(loadType == LoadType.REFRESH)
+                {
+                    Log.i(TAG, "load: REFRESH clearing remotekeys and stations")
+                    appDatabase.remoteKeyDao().clearRemoteKeys()
+                    appDatabase.stationDao().clearStations()
+                }
+                val prevKey = if (page == 1) null else page - 1
+                val nextKey = if (endOfPaginationReached) null else page+1
+                val keys = req.map {
+                    RemoteKeyEntity(
+                            stationPrimaryKey = ,
+
+                    )
+                }
+            }
+
 
 
         }catch (exception: IOException)
@@ -108,6 +148,38 @@ class StationRemoteMediator(
                 }
         Log.i(TAG, "getRemoteKeyForFirstItem: found remoteKeyEntity = $value")
         return value
+    }
+
+    private suspend fun getRemoteKeyForClosestItemToPosition(
+            state: PagingState<Int, StationResponse>
+    ):RemoteKeyEntity?
+    {
+        val value1 = state.anchorPosition?.let { safeAnchorPosition->
+            Log.i(TAG, "getRemoteKeyForClosestItemToPosition: safeAnchorPosition = $safeAnchorPosition")
+            val value2 = state.closestItemToPosition(safeAnchorPosition)?.stationPrimaryKey
+                    ?.let {safeStationPrimaryKey ->
+                        Log.i(TAG, "getRemoteKeyForClosestItemToPosition: safeStationPrimaryKey = $safeStationPrimaryKey")
+                        val remoteKeyEntity = appDatabase.remoteKeyDao().getRemoteKeysFromStationPrimaryKey(safeStationPrimaryKey)
+                        Log.i(TAG, "getRemoteKeyForClosestItemToPosition: " +
+                                "getRemoteKeysFromStationPrimaryKey lookup remoteKeyEntity " +
+                                "= $remoteKeyEntity")
+                        return@let remoteKeyEntity
+                    }?:let {
+                        Log.w(TAG, "getRemoteKeyForClosestItemToPosition: " +
+                                "getRemoteKeysFromStationPrimaryKey lookup found null")
+                        return@let null
+                    }
+            Log.i(TAG, "getRemoteKeyForClosestItemToPosition: closestItemToPosition(safeAnchorPosition) " +
+                    "(value2) found = $value2")
+            return@let value2
+        }?:let {
+            Log.w(TAG, "getRemoteKeyForClosestItemToPosition: closesItemToPosition(safeAnchorPosition) is null"  )
+            return@let null
+        }
+
+        Log.i(TAG, "getRemoteKeyForClosestItemToPosition: remoteKeyForClosestItemToPosition (value1) = $value1 ")
+
+        return value1
     }
 
 
