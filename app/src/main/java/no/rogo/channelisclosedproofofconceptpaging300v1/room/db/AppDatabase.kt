306876a33e92be795d5ea495011c93657b93f592
Roar <roar.gronmo@gmail.com>
2020-07-28 08:28:09 +0200

    Minor changes.

--- app/src/main/java/no/rogo/channelisclosedproofofconceptpaging300v1/room/db/AppDatabase.kt
+++ app/src/main/java/no/rogo/channelisclosedproofofconceptpaging300v1/room/db/AppDatabase.kt
@@ -23,7 +23,7 @@ import no.rogo.channelisclosedproofofconceptpaging300v1.room.entities.StationEnt
     StationEntity::class,
     DeviceLocationEntity::class,
     SearchLocationEntity::class],
-    version = 16,
+    version = 21,
     exportSchema = false)
 public abstract class AppDatabase:RoomDatabase()
 {
